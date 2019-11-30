43d849ef02b7b92ce6069ae3e389ee2033761df2
Haisky419 <Haisky419@gmail.com>
2019-11-30 10:50:27 +0700

    fin

---
+++ QLSV/Controllers/MonhocsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using D06_EFCoreCodeFirst.DataModels;
+using QuanLySinhVien.Models;
+
+namespace QLSV.Controllers
+{
+    public class MonhocsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public MonhocsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Monhocs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Monhocs.ToListAsync());
+        }
+
+        // GET: Monhocs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monhoc = await _context.Monhocs
+                .FirstOrDefaultAsync(m => m.MaMon == id);
+            if (monhoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(monhoc);
+        }
+
+        // GET: Monhocs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Monhocs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaMon,TenMon,SoTinChi")] Monhoc monhoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(monhoc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(monhoc);
+        }
+
+        // GET: Monhocs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monhoc = await _context.Monhocs.FindAsync(id);
+            if (monhoc == null)
+            {
+                return NotFound();
+            }
+            return View(monhoc);
+        }
+
+        // POST: Monhocs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaMon,TenMon,SoTinChi")] Monhoc monhoc)
+        {
+            if (id != monhoc.MaMon)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(monhoc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MonhocExists(monhoc.MaMon))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(monhoc);
+        }
+
+        // GET: Monhocs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monhoc = await _context.Monhocs
+                .FirstOrDefaultAsync(m => m.MaMon == id);
+            if (monhoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(monhoc);
+        }
+
+        // POST: Monhocs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var monhoc = await _context.Monhocs.FindAsync(id);
+            _context.Monhocs.Remove(monhoc);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MonhocExists(string id)
+        {
+            return _context.Monhocs.Any(e => e.MaMon == id);
+        }
+    }
+}
---
+++ QLSV/Controllers/SinhViensController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using D06_EFCoreCodeFirst.DataModels;
+using QuanLySinhVien.Models;
+
+namespace QLSV.Controllers
+{
+    public class SinhViensController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public SinhViensController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SinhViens
+        public async Task<IActionResult> Index()
+        {
+            var myDbContext = _context.SinhViens.Include(s => s.Khoas).Include(s => s.Lophocphans);
+            return View(await myDbContext.ToListAsync());
+        }
+
+        // GET: SinhViens/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sinhVien = await _context.SinhViens
+                .Include(s => s.Khoas)
+                .Include(s => s.Lophocphans)
+                .FirstOrDefaultAsync(m => m.MaSinhVien == id);
+            if (sinhVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(sinhVien);
+        }
+
+        // GET: SinhViens/Create
+        public IActionResult Create()
+        {
+            ViewData["MaKhoa"] = new SelectList(_context.Khoas, "MaKhoa", "MaKhoa");
+            ViewData["MaLHP"] = new SelectList(_context.Lophocphans, "MaLHP", "MaLHP");
+            return View();
+        }
+
+        // POST: SinhViens/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaSinhVien,HoTen,NgaySinh,DienThoai,MaKhoa,MaLHP")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sinhVien);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKhoa"] = new SelectList(_context.Khoas, "MaKhoa", "MaKhoa", sinhVien.MaKhoa);
+            ViewData["MaLHP"] = new SelectList(_context.Lophocphans, "MaLHP", "MaLHP", sinhVien.MaLHP);
+            return View(sinhVien);
+        }
+
+        // GET: SinhViens/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sinhVien = await _context.SinhViens.FindAsync(id);
+            if (sinhVien == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaKhoa"] = new SelectList(_context.Khoas, "MaKhoa", "MaKhoa", sinhVien.MaKhoa);
+            ViewData["MaLHP"] = new SelectList(_context.Lophocphans, "MaLHP", "MaLHP", sinhVien.MaLHP);
+            return View(sinhVien);
+        }
+
+        // POST: SinhViens/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaSinhVien,HoTen,NgaySinh,DienThoai,MaKhoa,MaLHP")] SinhVien sinhVien)
+        {
+            if (id != sinhVien.MaSinhVien)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sinhVien);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SinhVienExists(sinhVien.MaSinhVien))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKhoa"] = new SelectList(_context.Khoas, "MaKhoa", "MaKhoa", sinhVien.MaKhoa);
+            ViewData["MaLHP"] = new SelectList(_context.Lophocphans, "MaLHP", "MaLHP", sinhVien.MaLHP);
+            return View(sinhVien);
+        }
+
+        // GET: SinhViens/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sinhVien = await _context.SinhViens
+                .Include(s => s.Khoas)
+                .Include(s => s.Lophocphans)
+                .FirstOrDefaultAsync(m => m.MaSinhVien == id);
+            if (sinhVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(sinhVien);
+        }
+
+        // POST: SinhViens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var sinhVien = await _context.SinhViens.FindAsync(id);
+            _context.SinhViens.Remove(sinhVien);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SinhVienExists(string id)
+        {
+            return _context.SinhViens.Any(e => e.MaSinhVien == id);
+        }
+    }
+}
